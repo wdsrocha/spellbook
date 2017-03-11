0a4b5b2403d856b8c3666871b0fdbc3ebd123b8f
Wesley Rocha <wesleysr1997@gmail.com>
2017-03-11 19:28:10 -0400

    Added Template

---
+++ template.h
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define _ ios_base::sync_with_stdio(0);
+#define INF ((int)1e9)
+#define MOD7 (INF + 7)
+#define MOD11 (INF + 11)
+#define EPS (1e-9)
+#define PI (acos(-1))
+#define SZ(x) ((int)x.size())
+#define pb push_back
+#define fi first
+#define se second
+#define ull unsigned long long
+#define ll long long
+#define int ll
+#define ii pair <int, int>
+#define vi vector <int>
+#define vii vector <ii>
+#define vvi vector <vi>
+#define vvii vector <vii>
+#define dd pair <double, double>
+#define vd vector <double>
+#define vdd vector <dd>
+#define vvd vector <vd>
+#define vvdd vector <vdd>
+#define ALL(x) (x).begin(), (x).end()
+#define debug(x) cout<<#x<<" is "<<x<<endl;
+#define printList(x, y) for(int i=0;i<SZ(x);i++)cout<<setw(y)<<x[i];cout<<endl;
