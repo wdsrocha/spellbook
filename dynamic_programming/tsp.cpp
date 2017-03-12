5403fa7a91e02f123c23c00178de1c2c82fbf47a
Wesley Rocha <wesleysr1997@gmail.com>
2017-03-12 03:41:00 -0400

    fixed runtime error and added (glitchy) predecessor list

--- dynamic_programming/tsp.cpp
+++ dynamic_programming/tsp.cpp
@@ -1,29 +1,40 @@
 #include "../template.h"
 
 int n;
-vvi dist, memo;
+vi pred;
+vvd dist, memo;
 
-double tsp(int u, int mask)
-{
+double tsp(int u, int mask) {
 	if (mask == (1 << n) - 1) return dist[u][0];
-
 	if (memo[u][mask] != -1) return memo[u][mask];
-
 	double best = INF;
-	for (int v = 0; v < n; v++)
-		if (!(mask & (1 << v)))
-			best = min(best, tsp(v, mask | (1 << v)) + dist[u][v]);
-
+	for (int v = 0; v < n; v++) {
+		if (!(mask & (1 << v))) {
+			double cur = tsp(v, mask | (1 << v)) + dist[u][v];
+			if (cur < best) {
+				best = cur;
+				pred[u] = v;
+			}
+		}
+	}
 	return memo[u][mask] = best;
 }
 
-double tspCall()
-{
-	memo = vvi(n, vi((1 << n)));
-
+double tspCall() {
+	pred = vi(n);
+	memo = vvd(n, vd((1 << n)));
 	for (int i = 0; i < n; i++)
 		for (int j = 0; j < (1 << n); j++)
 			memo[i][j] = -1;
-
 	return tsp(0, 1);
+}
+
+vi printPath() {
+	vi path;
+	int u = 0;
+	for (int i = 0; i < n; i++) {
+		path.pb(pred[u]);
+		u = pred[u];
+	}
+	return path;
 }
