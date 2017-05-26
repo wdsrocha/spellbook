30e1141f8d337f37792852538d49049e9103d3ce
Wesley Rocha <wesleysr1997@gmail.com>
2017-05-26 19:38:17 -0400

    Fix

--- dynamic_programming/tsp.cpp
+++ dynamic_programming/tsp.cpp
@@ -5,8 +5,12 @@ vi pred;
 vvd dist, memo;
 
 double tsp(int u, int mask) {
-	if (mask == (1 << n) - 1) return dist[u][0];
-	if (memo[u][mask] != -1) return memo[u][mask];
+    if (mask == (1 << n) - 1) {
+        return dist[u][0];
+    }
+    if (memo[u][mask] != -1) {
+        return dist[u][0];
+    }
 	double best = INF;
 	for (int v = 0; v < n; v++) {
 		if (!(mask & (1 << v))) {
@@ -21,20 +25,11 @@ double tsp(int u, int mask) {
 }
 
 double tspCall() {
-	pred = vi(n);
 	memo = vvd(n, vd((1 << n)));
-	for (int i = 0; i < n; i++)
-		for (int j = 0; j < (1 << n); j++)
-			memo[i][j] = -1;
-	return tsp(0, 1);
-}
-
-vi printPath() {
-	vi path;
-	int u = 0;
 	for (int i = 0; i < n; i++) {
-		path.pb(pred[u]);
-		u = pred[u];
-	}
-	return path;
+		for (int j = 0; j < (1 << n); j++) {
+			memo[i][j] = -1;
+        }
+    }
+	return tsp(0, 1);
 }
