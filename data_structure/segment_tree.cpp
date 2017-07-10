1bc86d41d23d948a3ac0e3aef5a7d8ca4a90c0c0
Wesley Rocha <wesleysr1997@gmail.com>
2017-07-10 05:52:53 -0400

    Added new segment tree

--- data_structure/segment_tree.cpp
+++ data_structure/segment_tree.cpp
@@ -1,90 +1,71 @@
-// 0 p q v - você tem que adicionar v para todos os números na faixa de p à q (inclusive), onde p e q são dois índices do array.
-// 1 p q - imprima uma linha contendo um único inteiro, que é a soma de todos os elementos do array entre p e q (inclusive).
+// http://wcipeg.com/problem/segtree
 
 #include <bits/stdc++.h>
 using namespace std;
 
-#define _ ios_base::sync_with_stdio(0);
 #define INF ((int)1e9)
-#define pb push_back
-#define int long long
-#define ii pair <int, int>
-#define vi vector <int>
-#define vii vector <ii>
-#define vvi vector <vi>
-#define vvii vector <vii>
-#define left(x) (2 * x)
-#define right(x) (2 * x + 1)
-#define mid(a, b) ((a + b) / 2)
+#define MAX 100010
 
-int n, m, l, r, x;
-vi tree, lazy;
+int n, q, a[MAX], t[4*MAX];
 
-int query(int u = 1, int i = 1, int j = n) {
-	if (lazy[u]) {
-        if (i != j) {
-            lazy[left(u)] += lazy[u];
-            lazy[right(u)] += lazy[u];
-        }
-		tree[u] += (j - i + 1) * lazy[u];
-		lazy[u] = 0;
-	}
+int build(int u, int l, int r) {
+    if (l == r) {
+        return t[u] = a[l];
+    } 
 
-	if (i > r || j < l) return 0;
-
-	if (i >= l && j <= r) return tree[u];
-
-	int p1 = query(left(u), i, mid(i, j));
-	int p2 = query(right(u), mid(i, j) + 1, j);
-
-	return p1+p2;
+    build(2*u, l, (l+r)/2);
+    build(2*u+1, (l+r)/2+1, r);
+    return t[u] = min(t[2*u], t[2*u+1]);
 }
 
-int update(int u = 1, int i = 1, int j = n) {
-	if (lazy[u]) {
-        if (i != j) {
-            lazy[left(u)] += lazy[u];
-            lazy[right(u)] += lazy[u];
-        }
-		tree[u] += (j - i + 1) * lazy[u];
-		lazy[u] = 0;
-	}
+int query(int u, int l, int r, int i, int j) {
+    // 1. [l,r] está totalmente fora de [i, j]
+    if (r < i or l > j) {
+        return INF;
+    }
 
-	if (i > r || j < l) return tree[u];
+    // 2. [l,r] está dentro de [i, j]
+    if (l >= i and r <= j) {
+        return t[u];
+    }
 
-	if (i >= l && j <= r) {
-        if (i != j) {
-            lazy[left(u)] += x;
-            lazy[right(u)] += x;
-        }
-		return tree[u] += (j - i + 1) * x;
-	}
-
-	int p1 = update(left(u), i, mid(i, j));
-	int p2 = update(right(u), mid(i, j) + 1, j);
-
-	return tree[u] = p1 + p2;
+    // 3. [l, r] está parcialmente em [i, j]
+    int lside = query(2*u, l, (l+r)/2, i, j);
+    int rside = query(2*u+1, (l+r)/2+1, r, i, j);
+    return min(lside, rside);
 }
 
-signed main() {_
-	int tc;
-	cin >> tc;
-	while (tc--) {
-		cin >> n >> m;
+int update(int u, int l, int r, int target, int value) {
+    if (l == r) {
+        return t[u] = value;
+    }
 
-		tree = vi(5*n, 0);
-		lazy = vi(5*n, 0);
+    if (target <= (l+r)/2) {
+        update(2*u, l, (l+r)/2, target, value);
+    } else {
+        update(2*u+1, (l+r)/2+1, r, target, value);
+    }
+    
+    return t[u] = min(t[2*u], t[2*u+1]);
+}
 
-		while (m--) {
-			int cmd;
-			cin >> cmd >> l >> r;
-			if (l > r) swap(l, r);
-			if (!cmd) {
-				cin >> x;
-				update();
-			}
-			else cout << query() << endl;
-		}
-	}
-	return 0;
+int main() {
+    while (cin >> n >> q) {
+        for (int i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+        build(1, 0, n-1);
+        while (q--) {
+            char cmd;
+            int i, j;
+            cin >> cmd >> i >> j;
+            if (cmd == 'Q') {
+                cout << query(1, 0, n-1, i, j) << endl;
+            } else {
+                update(1, 0, n-1, i, j);
+            }
+        }
+    }
+
+    return 0;
 }
