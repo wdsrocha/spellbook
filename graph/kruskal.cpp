8856e567610ce82ea9b6ffc17264bfb4c4d51fb2
Wesley Rocha <wesleysr1997@gmail.com>
2017-09-05 19:07:41 -0400

    Adicionado kruskal

--- graph/kruskal.cpp
+++ graph/kruskal.cpp
@@ -1,1 +1,50 @@
-"" 
+#include "./../template.h"
+
+int n, m;
+
+struct Edge {
+    int u, v;
+    double w;
+};
+
+vi p, sz;
+vector <Edge> e;
+
+void init() {
+    vi p, sz;
+    for (int i = 0; i <= n; i++) {
+        p.pb(i);
+        sz.pb(1);
+    }
+}
+
+int find(int x) {
+    return x == p[x] ? x : p[x] = find(p[x]);
+}
+
+bool join(int u, int v) {
+    u = find(u);
+    v = find(v);
+    if (u == v) {
+        return false;
+    }
+
+    if (sz[u] < sz[v]) {
+        swap(u, v);
+    }
+    
+}
+    
+
+int kruskal() {
+    int ans = 0;
+    for (int i = 0, j = 1; i < n && j < m; i++) {
+        int u = e[i].u;
+        int v = e[i].v;
+        double w = e[i].w;
+        if (join(u, v)) {
+            ans += w;
+    }
+    return ans;
+}
+
